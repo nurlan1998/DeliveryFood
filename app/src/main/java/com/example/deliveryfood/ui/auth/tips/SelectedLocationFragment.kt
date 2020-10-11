e8038a07fc28b9608d35da118d54b16bed2d65d3
nurlan1998 <nurlanungarbaev98@gmail.com>
2020-10-11 11:34:53 +0500

    refactoring code

--- app/src/main/java/com/example/deliveryfood/ui/auth/tips/SelectedLocationFragment.kt
+++ app/src/main/java/com/example/deliveryfood/ui/auth/tips/SelectedLocationFragment.kt
@@ -10,12 +10,8 @@ import android.location.Location
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
-import android.view.KeyEvent
 import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.TextView
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.deliveryfood.R
@@ -30,7 +26,7 @@ import java.lang.Exception
 import java.util.*
 import kotlin.collections.ArrayList
 
-class SelectedLocationFragment : Fragment(R.layout.fragment_selected_location),OnMapReadyCallback{
+class SelectedLocationFragment : Fragment(R.layout.fragment_selected_location), OnMapReadyCallback {
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val locationRequestId = 100
@@ -39,19 +35,28 @@ class SelectedLocationFragment : Fragment(R.layout.fragment_selected_location),O
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mapView.onCreate(savedInstanceState)
-        mapView.onResume()
-        mapView.getMapAsync(this)
-        getLocation()
-
         fusedLocationProviderClient = FusedLocationProviderClient(requireActivity())
+
+        mapView.onCreate(savedInstanceState)
 
         btnSaveAddress.setOnClickListener {
             findNavController().navigate(R.id.action_selectedLocationFragment3_to_addressFragment3)
         }
     }
 
-    var mLocationCallback = object : LocationCallback(){
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+    }
+
+    private fun getLocation() {
+        if (checkForLocationPermission()) {
+            updateLocation()
+        } else {
+            askLocationPermission()
+        }
+    }
+
+    var mLocationCallback = object : LocationCallback() {
         override fun onLocationResult(result: LocationResult) {
 
             val location: Location = result.lastLocation
@@ -59,72 +64,60 @@ class SelectedLocationFragment : Fragment(R.layout.fragment_selected_location),O
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        getLocation()
-    }
-
-    private fun getLocation(){
-        if(checkForLocationPermission()){
-            updateLocation()
-        }else{
-            askLocationPermission()
-        }
-    }
-
-    private fun updateLocation(){
+    private fun updateLocation() {
         val locationRequest = LocationRequest()
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         locationRequest.interval = 1000
         locationRequest.fastestInterval = 5000
 
-        fusedLocationProviderClient = FusedLocationProviderClient(requireActivity())
-        if(checkForLocationPermission()){
-            fusedLocationProviderClient.requestLocationUpdates(locationRequest,mLocationCallback,
-                Looper.myLooper())
+        fusedLocationProviderClient = FusedLocationProviderClient(requireContext())
+        if (checkForLocationPermission()) {
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest, mLocationCallback,
+                Looper.myLooper()
+            )
         }
     }
-//
-//    private fun init(){
-//        etSearch.setOnEditorActionListener { textView, i, keyEvent ->
-//            if(i == EditorInfo.IME_ACTION_SEARCH
-//                || i == EditorInfo.IME_ACTION_DONE
-//                || keyEvent.action == KeyEvent.ACTION_DOWN
-//                || keyEvent.action == KeyEvent.KEYCODE_ENTER) {
-//
-//            }
-//            return@setOnEditorActionListener false
-//            }
-//        }
 
-    fun updateAddressUI(location: Location){
+    fun updateAddressUI(location: Location) {
         val addressList: ArrayList<Address>
         try {
             val gecoder = Geocoder(requireContext(), Locale.getDefault())
-            addressList = gecoder.getFromLocation(location.latitude,location.longitude,1) as ArrayList<Address>
+            addressList = gecoder.getFromLocation(
+                location.latitude,
+                location.longitude,
+                1
+            ) as ArrayList<Address>
 
+            tvAddressLocation.text = addressList[0].getAddressLine(0)
 
-            tvAddressLocation.text = (addressList[0].getAddressLine(0)).toString()
-
-            val latLng = LatLng(location.latitude,location.longitude)
-            Log.e("MyLocation",location.longitude.toString() + " " + location.latitude.toString())
+            val latLng = LatLng(location.latitude, location.longitude)
+            Log.e("MyLocation", location.longitude.toString() + " " + location.latitude.toString())
 
             val marker = MarkerOptions().position(latLng)
-            mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f))
+            mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
             mGoogleMap.addMarker(marker)
-        }catch (e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
-    private fun checkForLocationPermission(): Boolean{
-        if(ActivityCompat.checkSelfPermission(requireContext(),Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-        return true
+    private fun checkForLocationPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+            return true
         return false
     }
 
-    private fun askLocationPermission(){
-        ActivityCompat.requestPermissions(requireContext() as Activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),locationRequestId)
+    private fun askLocationPermission() {
+        ActivityCompat.requestPermissions(
+            requireContext() as Activity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            locationRequestId
+        )
     }
 
     override fun onRequestPermissionsResult(
@@ -133,44 +126,54 @@ class SelectedLocationFragment : Fragment(R.layout.fragment_selected_location),O
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == locationRequestId && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                getCurrentLocation()
+        if (requestCode == locationRequestId && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             getLocation()
         }
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mGoogleMap = googleMap
+    private fun initGoogleMap() {
+        mapView.getMapAsync(this)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-            getLocation()
+    private fun initMap() {
+        if (checkForLocationPermission()) {
+            initGoogleMap()
+        }
     }
-    //
-//    private fun getCurrentLocation(){
-//
-//        if (ActivityCompat.checkSelfPermission(
-//                requireContext(),
-//                Manifest.permission.ACCESS_FINE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-//                requireContext(),
-//                Manifest.permission.ACCESS_COARSE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            return
-//        }
-//        var task = fusedLocationProviderClient.lastLocation
-//        task.addOnSuccessListener {location ->
-//            if(location != null){
-//                mapView.getMapAsync{ googleMap ->
-//                    val latLng = LatLng(location.latitude,location.longitude)
-//                    Log.e("MyLocation",location.longitude.toString() + location.latitude.toString())
-//                    val marker = MarkerOptions().position(latLng)
-//                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f))
-//                    googleMap.addMarker(marker)
-//                }
-//            }
-//        }
-//    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getLocation()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initMap()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
 }
