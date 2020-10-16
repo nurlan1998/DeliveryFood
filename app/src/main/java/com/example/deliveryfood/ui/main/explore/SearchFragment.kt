1397af4624beca418f3f8ca9508651b14f02adf7
jalgas93 <jalgas1993@list.ru>
2020-10-16 14:22:20 +0500

    added expandable in detail
    # Conflicts:
    #	app/src/main/java/com/example/deliveryfood/ui/main/detail/DetailFragment.kt
    #	app/src/main/res/layout/fragment_detail.xml

--- app/src/main/java/com/example/deliveryfood/ui/main/explore/SearchFragment.kt
+++ app/src/main/java/com/example/deliveryfood/ui/main/explore/SearchFragment.kt
@@ -1,7 +1,19 @@
 package com.example.deliveryfood.ui.main.explore
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.deliveryfood.R
+import kotlinx.android.synthetic.main.fragment_explore.*
 
 class SearchFragment : Fragment(R.layout.fragment_explore) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn.setOnClickListener {
+            findNavController().navigate(R.id.action_exploreFragment_to_detailFragment)
+        }
+    }
+
+
 }
