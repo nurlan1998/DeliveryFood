6cfa950606bc4affccb5ff980dba4f4efd7b0c5a
nurlan1998 <nurlanungarbaev98@gmail.com>
2020-10-16 14:35:36 +0500

    added expandable in detail
    # Conflicts:
    #	app/src/main/java/com/example/deliveryfood/ui/main/detail/DetailFragment.kt
    #	app/src/main/res/layout/fragment_detail.xml

---
+++ app/src/main/java/com/example/deliveryfood/ui/main/detail/NewspaperRecyclerAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.deliveryfood.ui.main.detail
+
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.deliveryfood.R
+import com.example.deliveryfood.ui.main.detail.model.NewspaperModel
+
+class NewspaperAdapter(context: Context, data: List<NewspaperModel>?) :
+    RecyclerView.Adapter<NewspaperAdapter.NewspaperViewHolder>() {
+    private var mContext: Context = context
+    private var items: List<NewspaperModel>? = data
+    private var inflater: LayoutInflater = LayoutInflater.from(context)
+    private var headlineAdapter: HeadlinesAdapter? = null
+
+    override
+    fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewspaperViewHolder {
+        val view = inflater.inflate(R.layout.item_news_paper, parent, false)
+        return NewspaperViewHolder(view)
+    }
+
+    override
+    fun onBindViewHolder(holder: NewspaperViewHolder, position: Int) {
+        val item = items?.get(position)
+
+        holder.tvName.text = item?.papername
+        headlineAdapter = HeadlinesAdapter(mContext, item?.paperHeadlinesModel)
+        holder.rvHeadlines.adapter = headlineAdapter
+        holder.rvHeadlines.layoutManager = LinearLayoutManager(mContext)
+        holder.itemView.setOnClickListener { onItemClicked(item) }
+        if (item?.isExpanded!!) {
+            holder.rvHeadlines.visibility = View.VISIBLE
+            holder.ivArrow.setImageResource(R.drawable.ic_arrow_up)
+        } else {
+            holder.rvHeadlines.visibility = View.GONE
+            holder.ivArrow.setImageResource(R.drawable.ic_arrow_down)
+        }
+    }
+
+    override
+    fun getItemCount(): Int {
+        return items?.size ?: 0
+    }
+
+    private fun onItemClicked(newspaperModel: NewspaperModel?) {
+        newspaperModel?.isExpanded = !newspaperModel?.isExpanded!!
+        notifyDataSetChanged()
+    }
+
+    class NewspaperViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvName: TextView = itemView.findViewById(R.id.tvPaperName)
+        var rvHeadlines: RecyclerView = itemView.findViewById(R.id.rvHeadlines)
+        var ivArrow: ImageView = itemView.findViewById(R.id.ivArrow)
+    }
+}
