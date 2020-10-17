fb085aacb0f73adc5e3507e72c800ebaf8af3fba
nurlan1998 <nurlanungarbaev98@gmail.com>
2020-10-17 14:13:39 +0500

    add design cart
    # Conflicts:
    #	app/src/main/res/layout/fragment_cart.xml

--- app/src/main/java/com/example/deliveryfood/ui/main/detail/DetailFragment.kt
+++ app/src/main/java/com/example/deliveryfood/ui/main/detail/DetailFragment.kt
@@ -9,7 +9,6 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.deliveryfood.R
 import com.example.deliveryfood.ui.main.detail.model.JsonHelper
@@ -73,9 +72,5 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 //                dialog.show()
 //
 //            }
-
-        }
-
-    }
 
 }
